09a5f503c5429b53549c05ba523f6d875b70821e
Seth Corbett <scorbz9@gmail.com>
2022-02-04 15:38:52 -0800

    wrote patch review route, able to edit reviews using postman

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, Response, request
 from app.models import db, Review
 import json
+import datetime
 
 
 review_routes = Blueprint('reviews', __name__)
@@ -23,10 +24,21 @@ def post_review(business_id):
     )
 
     db.session.add(new_review)
+
     db.session.commit()
 
-    return 'test'
+    return 'test post'
 
 @review_routes.route('/<int:review_id>', methods=["PATCH"])
 def edit_review(business_id, review_id):
     data = json.loads(request.data)
+
+    review_to_update = Review.query.filter(Review.id == review_id).one()
+
+    review_to_update.rating = data["rating"]
+    review_to_update.review = data["review"]
+    review_to_update.time_updated = datetime.datetime.now()
+
+    db.session.commit()
+
+    return 'test patch'
