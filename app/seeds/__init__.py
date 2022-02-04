8e567565c14f98cfae6a64a38672dee85be89db0
Thien Dang <ctap105@gmail.com>
2022-02-04 13:56:21 -0500

    added photos to init seeder

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .businesses import seed_businesses, undo_businesses
+from .photos import seed_photos, undo_photos
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -12,6 +13,7 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_users()
     seed_businesses()
+    seed_photos()
     # Add other seed functions here
 
 
@@ -20,4 +22,5 @@ def seed():
 def undo():
     undo_users()
     undo_businesses()
+    undo_photos()
     # Add other undo functions here
