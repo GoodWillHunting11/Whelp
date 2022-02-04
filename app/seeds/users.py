69d86c0c5db5d5e421798805e40c17fdd14bff4b
Thien Dang <ctap105@gmail.com>
2022-02-04 11:55:57 -0500

    added user seeds

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,11 +4,11 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', email='demo@aa.io', password='password', role='admin', zipcode='83646')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie', email='marnie@aa.io', password='password', role='member', zipcode='83646')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='bobbie', email='bobbie@aa.io', password='password', role='member', zipcode='83646')
 
     db.session.add(demo)
     db.session.add(marnie)
