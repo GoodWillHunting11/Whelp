3c3cf08bd21fc8930b744bb151b6b6454ebe62d8
Seth Corbett <scorbz9@gmail.com>
2022-02-08 13:23:40 -0800

    category routes + category store + category test component

---
+++ app/api/category_routes.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, jsonify, session, request
+from flask_login import login_required
+from app.models import Business, Category, Photo, Map, db, Review, User
+from sqlalchemy.orm import joinedload
+
+
+category_routes = Blueprint('categories', __name__)
+
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
+
+@category_routes.route('/', methods=['GET'])
+def fetch_all():
+    print('jeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeelo')
+    all_cats = Category.query.options(joinedload(Category.business)).all()
+    print('heeeeeeeeelo', all_cats)
+    data = []
+    for cat in all_cats:
+        print('eaaaaaaaaaaaach one', cat)
+        business_data = get_business(cat.business)
+        each = {
+            "id": cat.id,
+            "category": cat.name,
+            "businesses": business_data
+        }
+        data.append(each)
+    return {"data":data}
+
+def get_business(business):
+    data = []
+    for each in business:
+        business_set = {
+            "id": each.id,
+            "name": each.name,
+            "address": each.address,
+            "city": each.city,
+            "state": each.state,
+            "zipcode": each.zipcode,
+            "phone": each.phone,
+            "website": each.website
+        }
+        data.append(business_set)
+    return data
---
+++ react-app/src/store/category.js
@@ -0,0 +1,38 @@
+const LOAD_BUSINESSCAT = 'businessesCat/LOAD_BUSINESSCAT'
+
+
+export const loadBusinessesCat = payload => {
+    return {
+        type: LOAD_BUSINESSCAT,
+        payload
+    }
+}
+
+
+export const getAllBusinessesCat = () => async dispatch => {
+
+    const response = await fetch(`/api/categories`)
+    console.log(response, "respuestaaaaaaaaaaama")
+
+    if (response.ok) {
+        const businessesCat = await response.json()
+        dispatch(loadBusinessesCat(businessesCat))
+    }
+}
+
+
+export const initialState = { entries: [] }
+
+const businessCatReducer = ( state = initialState, action ) => {
+    let newState
+    switch (action.type) {
+        case LOAD_BUSINESSCAT:
+            return {...state, entries: [...action.payload['data']]}
+
+        default:
+            return state
+    }
+}
+
+
+export default businessCatReducer
