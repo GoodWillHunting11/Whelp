ffbef572580efe4b84a42aab653df45f84510dd6
Seth Corbett <scorbz9@gmail.com>
2022-02-08 15:41:16 -0800

    fixed warnings for bad jsx

--- react-app/src/components/authenticated/SingleBusiness/ReviewSection/ReviewDropDown/index.js
+++ react-app/src/components/authenticated/SingleBusiness/ReviewSection/ReviewDropDown/index.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Redirect, Link, useHistory } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './ReviewDropDown.css'
 
 
@@ -8,9 +7,9 @@ const ReviewDropDown = ({ review, handleDeleteReview, id}) => {
 
 
     return (
-        <div class="dropdown-container" tabindex="-1">
-                        <div class="three-dots"></div>
-                        <div class="review-dropdown">
+        <div className="dropdown-container" tabIndex="-1">
+                        <div className="three-dots"></div>
+                        <div className="review-dropdown">
                             <Link className='review-button' to={`/businesses/${id}/reviews/${review.id}/edit`}>Edit</Link>
                             <form onSubmit={handleDeleteReview} id={`${review.id}`}>
                                 <button className='review-button' type="submit">Delete</button>
