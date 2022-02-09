4b87eb220de546ae1cfa5f17db2af6a2b2d2e3e1
Seth Corbett <scorbz9@gmail.com>
2022-02-09 15:56:34 -0800

    andres work

--- react-app/src/components/authenticated/BusinessCat/index.js
+++ react-app/src/components/authenticated/BusinessCat/index.js
@@ -1,55 +1,65 @@
-import { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useParams, useHistory, NavLink, Link } from 'react-router-dom'
-import { getAllBusinessesCat } from '../../../store/business';
-
+import { useSelector } from 'react-redux'
+import { useParams, Link } from 'react-router-dom'
+import './BusinessCat.css'
 
 
 
 const BusinessCat = () => {
-    const dispatch = useDispatch()
-    const history = useHistory()
     const { id } = useParams()
-    const { business_id } = useParams()
     const user = useSelector(state => state.session.user)
     const businesses = useSelector(state => state.businessState.entries)
-    console.log(businesses, "businesssssssssssss")
+    const selectedBusinesses = businesses.filter(business => business.categories[0].id === +id)
+    const single = businesses.find(single => single.id === +id)
+    // const businessescat = useSelector(state => state.businessCatState.entries)
+    // const singleCat = businessescat.find(single => single.id === +id)
 
 
-    const single = businesses.find(single => single.id === +3)
 
+    let rating = 0;
+    const ratings = single?.reviews?.map(review => review.rating)
+    if (ratings?.length) {
+        ratings?.forEach(rate => rating = rate + rating)
+        rating = rating / ratings.length
+    }
 
-    const businessescat = useSelector(state => state.businessCatState.entries)
-    console.log(businessescat, "businesss catttttttttttt")
-    const singleCat = businessescat.find(single => single.id === +id)
-    console.log(singleCat, "singleeeecatttttttt")
-
-    const groomers = businessescat.filter(category => category.id === 3)
-
-    const feed = Object.values(groomers)
-    console.log(singleCat?.businesses, "feeeeeeedddddd")
-
-
+    const formatPhone = (number) => {
+        return number.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3")
+    }
 
     return (
-        <div className='single-business-container'>
-        <h2>Business Categories</h2>
-        <>
-        {singleCat?.businesses?.map (business => (
-            <h1 className='test'>{business.name}</h1>
-        )) }
+        <div className='main-cat-container'>
+            <div className='search-title-container'>
+
+                    <h1 className='search-stream-title' >Category: <span className="search-subtitle">{single?.categories[0]?.category}</span></h1>
+
+            </div>
+            <div className='business-roll'>
+                <>
+                    {selectedBusinesses?.map(business => (
+                        <div className='roll-container'>
+                            {business.photos.length > 1 ? <div className='roll-pic' style={{ backgroundImage: `url(${business?.photos[0]?.url})` }}> </div> : <div className='roll-no-pic'>
+
+                            </div>}
 
 
+                            <div className='roll-data'>
+                                <Link className='roll-title' to={`/businesses/${business?.id}`}>{business?.name}</Link>
+                                <p className="roll-info"><strong>Category:</strong> {business?.categories[0]?.category}</p>
+                                <p className="roll-info"><strong>Contact:</strong> {formatPhone(business.phone)}</p>
+                                <p className="roll-info"><strong>Address:</strong> {business?.address} {business?.city}, {business?.state} {business?.zipcode}</p>
+                                <div className='rating-container'>
+                                    <div className='roll-rating'><span className="stars" style={{ "--rating": `${rating}` }}></span></div>
+                                    <div className='roll-more'><Link className='roll-more-link' to={`/businesses/${business?.id}`}>MORE...</Link></div>
 
+                                </div>
+                            </div>
 
+                        </div>
+                    ))}
 
-
-        </>
-
-    </div>
-
-
-
+                </>
+            </div>
+        </div>
     )
 
 }
