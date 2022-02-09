f3d11287e6d84dfa458b78d7681cb3eead1208ff
Seth Corbett <scorbz9@gmail.com>
2022-02-09 09:19:28 -0800

    made style tweaks (more underline + search submit spacing)

--- react-app/src/components/authenticated/Hero/index.js
+++ react-app/src/components/authenticated/Hero/index.js
@@ -1,4 +1,4 @@
-import { Link, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaw, faStethoscope, faCut, faDog, faBone, faBell } from '@fortawesome/free-solid-svg-icons'
 
