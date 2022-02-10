e8a84be40ae9470c75177f8830357d9baadc3142
Thien Dang <ctap105@gmail.com>
2022-02-10 09:08:04 -0500

    limit business roll to 8

--- react-app/src/components/authenticated/index.js
+++ react-app/src/components/authenticated/index.js
@@ -11,6 +11,27 @@ import './App.css'
 const HomeApp = () => {
 
     const businesses = useSelector(state => state.businessState.entries)
+    let longRoll = []
+    if (businesses[7]) {
+        for (let i = 0; i < 8; i++) {
+            longRoll.push(businesses[i])
+        }
+    }
+
+    if (longRoll[1]) {
+        return (
+            <>
+                <HeroImage />
+                <h1 className='roll-heading'>Your Next Review Awaits</h1>
+                <div className='business-roll'>
+                    {longRoll.map((biz, idx) => (
+                        <BusinessRoll biz={biz} key={idx} />
+                    ))}
+                </div>
+            </>
+    )
+    }
+
 
     return (
             <>
